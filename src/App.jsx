5be68ab2ee76f2f5785f0b23ee2ecf195f640150
João Victor <domjuan45@eaportal.org>
2024-03-27 22:02:25 -0300

    Projeto finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import Sedans from "./Components/Sedans"
 import Suvs from "./Components/Suvs"
-import Luxo from "./Components/Luxo"
+import Luxuosos from "./Components/Luxo"
 
 
 export default function App() {
   return (
     <main className="min-h-svh bg-offwhitebg py-20 px-6 antialiased lg:flex justify-center items-center">
-      <section className="lg:flex max-w-[920px]">
+      <div className="lg:flex max-w-[920px]">
         <Sedans/>
         <Suvs/>
-        <Luxo/>
-      </section>
+        <Luxuosos/>
+      </div>
     </main>
   )
 }
--- src/Components/Luxo.jsx
+++ src/Components/Luxo.jsx
@@ -1,8 +1,8 @@
 import iconLuxury from "../assets/icon-luxury.svg"
 
-export default function Luxo(){
+export default function Luxuosos(){
     return(
-        <div className="bg-dark-green p-12 max-lg:rounded-b-lg lg:rounded-r-lg">
+        <section className="bg-dark-green p-12 max-lg:rounded-b-lg lg:rounded-r-lg">
           <img src={iconLuxury} alt="Ìcones dos carros Luxo" className="mb-7"/>
           <h2 className="font-shoulders mb-7 uppercase text-4xl font-bold text-offwhitebg">Luxuosos</h2>
           <p className="font-lexend mb-7 text-offwhite lg:mb-20">Passeie nos melhores carros sem os
@@ -11,6 +11,6 @@ export default function Luxo(){
           <a className="font-lexend bg-offwhitebg block px-10 py-3 w-fit rounded-3xl text-dark-green 
           border-2 border-offwhitebg hover:bg-dark-green hover:text-offwhitebg" 
           href="https://www.webmotors.com.br/carros-de-luxo" target="_blank">Ver mais</a>
-        </div>
+        </section>
     )
 }
