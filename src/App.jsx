c038294b4d1620b3905da59e1a0f99e2a6429994
João Victor <domjuan45@eaportal.org>
2024-03-27 21:07:34 -0300

    primeiro commit da responsividade

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import iconLuxury from "./assets/icon-luxury.svg"
 
 export default function App() {
   return (
-    <main className="min-h-svh bg-offwhitebg py-20 px-6">
-      <section>
+    <main className="min-h-svh bg-offwhitebg py-20 px-6 antialiased">
+      <section className="lg:flex justify-center items-center">
         <div className="bg-orange p-12 rounded-t-lg">
           <img src={iconSedans} alt="Ìcone dos carros sedan" className="mb-7"/>
           <h2 className="font-shoulders mb-7 uppercase text-4xl font-bold text-offwhitebg">Sedans</h2> 
